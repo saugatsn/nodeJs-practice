b4b01c86094e4e5d10e6c2b709c5d0b543e3c056
saugatsn <nepal.saugat.sn@gmail.com>
2024-01-02 12:46:19 +0545

    exports semi final

--- math.js
+++ math.js
@@ -1,13 +1,5 @@
-const sum=(a,b)=>a+b;
-const mul=(a,b)=>a*b;
-const div=(a,b)=>a/b;
-const sub=(a,b)=>a-b;
-const pi=3.14;
-let obj={
-    sum:sum,
-    mul:mul,
-    div:div,
-    sub:sub,
-    pi:pi
-};
-module.exports=obj;
+module.exports.sum=(a,b)=>a+b;
+module.exports.mul=(a,b)=>a*b;
+module.exports.div=(a,b)=>a/b;
+module.exports.sub=(a,b)=>a-b;
+module.exports.pi=3.14;
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let math=require("./math")
+let math=require("./math");
+console.log(math);
 console.log(math.sum(1,5));
 console.log(math.sub(4,1));
 console.log(math.pi);
