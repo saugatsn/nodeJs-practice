fbdd40304e7baca91f5c7ff1a0e1de07ab18c51b
saugatsn <nepal.saugat.sn@gmail.com>
2024-01-02 12:52:22 +0545

    exports final

--- math.js
+++ math.js
@@ -1,5 +1,5 @@
-module.exports.sum=(a,b)=>a+b;
-module.exports.mul=(a,b)=>a*b;
-module.exports.div=(a,b)=>a/b;
-module.exports.sub=(a,b)=>a-b;
-module.exports.pi=3.14;
+exports.sum=(a,b)=>a+b;
+exports.mul=(a,b)=>a*b;
+exports.div=(a,b)=>a/b;
+exports.sub=(a,b)=>a-b;
+exports = 3.14; //will act as variable
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let math=require("./math");
 console.log(math);
-console.log(math.sum(1,5));
-console.log(math.sub(4,1));
-console.log(math.pi);
+// console.log(math.sum(1,5));
+// console.log(math.sub(4,1));
+// console.log(math.pi);
