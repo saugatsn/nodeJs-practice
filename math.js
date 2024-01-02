c70c4845806439edb304558a0303a872397069bf
saugatsn <nepal.saugat.sn@gmail.com>
2024-01-02 13:17:26 +0545

    exports from directory having multiple files

--- math.js
+++ math.js
@@ -1,5 +1,5 @@
-exports.sum=(a,b)=>a+b;
-exports.mul=(a,b)=>a*b;
-exports.div=(a,b)=>a/b;
-exports.sub=(a,b)=>a-b;
+// exports.sum=(a,b)=>a+b;
+// exports.mul=(a,b)=>a*b;
+// exports.div=(a,b)=>a/b;
+// exports.sub=(a,b)=>a-b;
 exports = 3.14; //will act as variable
--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-let math=require("./math");
-console.log(math);
-// console.log(math.sum(1,5));
-// console.log(math.sub(4,1));
-// console.log(math.pi);
+let fruits=require("./fruits");
+console.log(fruits);
